2550d9ab689c7822287b5b2b727061f66ffee0cc
S444H <2030408929@qq.com>
2025-07-13 22:59:11 +0800

    优化与更改 2025/7/14

--- RL_Advanced_Algorithms/rl_utils.py
+++ RL_Advanced_Algorithms/rl_utils.py
@@ -32,7 +32,8 @@ def train_on_policy_agent(env, agent, num_episodes):
                 transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
                 state, info = env.reset()
                 done = False
-                while not done:
+                truncated = False
+                while not (done or truncated):  # 杆子倒下或达到最大步数
                     action = agent.take_action(state)
                     next_state, reward, done, truncated, _ = env.step(action)  # Gymnasium返回值不一样
                     transition_dict['states'].append(state)
@@ -53,21 +54,24 @@ def train_on_policy_agent(env, agent, num_episodes):
 # 训练+进度条,复用代码(离线策略)
 def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
     return_list = []
+    # 分10个进度条
     for i in range(10):
         with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
             for i_episode in range(int(num_episodes/10)):
                 episode_return = 0
                 state, info = env.reset()
                 done = False
-                while not done:
+                truncated = False
+                while not (done or truncated):  # 杆子倒下或达到最大步数
                     action = agent.take_action(state)
                     next_state, reward, done, truncated, _ = env.step(action)  # Gymnasium返回值不一样
                     replay_buffer.add(state, action, reward, next_state, done)
                     state = next_state
                     episode_return += reward
-                    # 满足最小容量则可随机抽取
+                    # 累积完最小经验容量则可随机抽取
                     if replay_buffer.size() > minimal_size:
                         b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                        # 依据抽取的经验字典更新网络参数
                         transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r, 'dones': b_d}
                         agent.update(transition_dict)
                 return_list.append(episode_return)
@@ -77,12 +81,14 @@ def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size
     return return_list
 
 
+# 废弃
 def moving_average(a, window_size):
     cumulative_sum = np.cumsum(np.insert(a, 0, 0))  # 使用积累和(插入0使得累积和从0开始)，提高计算效率&
     middle = (cumulative_sum[window_size:] - cumulative_sum[:-window_size]) / window_size  # 计算中间部分的滑动平均
     return middle  # 仅保留中间部分
 
 
+#
 def compute_advantage(gamma, lmbda, td_delta):
     td_delta = td_delta.detach().numpy()
     advantage_list = []
