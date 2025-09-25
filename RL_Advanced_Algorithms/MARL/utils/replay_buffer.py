3e42f986732ba22c5b683952af5ea1897897ce3e
S444H <2030408929@qq.com>
2025-09-25 18:36:36 +0800

    2025/9/25

---
+++ RL_Advanced_Algorithms/MARL/utils/replay_buffer.py
@@ -0,0 +1,27 @@
+import collections
+import random
+import numpy as np
+
+
+# 经验回放池，复用代码
+class ReplayBuffer:
+    """ 经验回放池 """
+
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)  # 队列,先进先出
+
+    def add(self, state, action, reward, next_state, done):  # 将数据加入buffer
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):  # 从buffer中采样数据,数量为batch_size
+        transitions = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = zip(*transitions)
+        return np.array(state), np.array(action), reward, np.array(next_state), done
+
+    def sample1(self, batch_size):  # 从buffer中采样数据,数量为batch_size
+        transitions = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = zip(*transitions)
+        return state, action, reward, next_state, done
+
+    def size(self):  # 目前buffer中数据的数量
+        return len(self.buffer)
